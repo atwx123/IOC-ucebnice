2825f8f8211c025b948263c6e248218b50fa22a2
atwx123 <alice.dedicova@gmail.com>
2025-11-06 19:13:38 +0100

    problem with ts

--- index.ts
+++ index.ts
@@ -1,7 +1,24 @@
-function greeter1(person: string) : string {
-    return "Hello, " + person;
-  }
-   
-  let user1 : string = "Jane User";
-   
-  document.body.textContent = greeter1(user1);
+import './style.css';
+
+let canvas: HTMLCanvasElement;
+let ctx: CanvasRenderingContext2D;
+
+let width : number = window.innerWidth;
+let height : number = window.innerHeight;
+
+canvas = document.getElementById('lomSvetlaInt') as HTMLCanvasElement;
+ctx = canvas.getContext('2d') as CanvasRenderingContext2D;
+
+canvas.width = width;
+canvas.height = height;
+
+draw();
+
+console.log (canvas.getContext('2d'));
+
+function draw() {
+  ctx.fillStyle = 'lightblue';
+  ctx.fillRect(0, 0, width, height/2);
+  ctx.fillStyle = 'lightgreen';
+  ctx.fillRect(0, height/2, width, height/2);
+};
