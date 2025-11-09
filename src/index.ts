6104b0420ed8a5720d2a1bf2f4621b282b11299c
atwx123 <alice.dedicova@gmail.com>
2025-11-09 16:28:15 +0100

    animation

--- src/index.ts
+++ src/index.ts
@@ -6,48 +6,58 @@ let ctx: CanvasRenderingContext2D;
 let width: number = 1000;
 let height: number = 600;
 
-canvas = document.getElementById('lomSvetlaInt') as HTMLCanvasElement;
-ctx = canvas.getContext('2d') as CanvasRenderingContext2D;
-let angleTextInput: HTMLInputElement = document.getElementById('angleText') as HTMLInputElement;
-let angleSendButton: HTMLButtonElement = document.getElementById('angleSend') as HTMLButtonElement;
+canvas = document.getElementById("lomSvetlaInt") as HTMLCanvasElement;
+ctx = canvas.getContext("2d") as CanvasRenderingContext2D;
+let angleTextInput: HTMLInputElement = document.getElementById(
+  "angleText"
+) as HTMLInputElement;
+let angleSendButton: HTMLButtonElement = document.getElementById(
+  "angleSend"
+) as HTMLButtonElement;
 
-let angleValue: number, NaN = -1;
+let angleValue: number, NaN;
+angleValue = -1;
 
-function readingAngleValue (angleTextInput : HTMLInputElement) {
+function readingAngleValue(angleTextInput: HTMLInputElement) {
   angleValue = parseFloat(angleTextInput.value);
-  if (!(angleValue != NaN && angleValue < 90 && angleValue > -1)) {
-    alert("Zadejte prosim cislo od 0 do 90")
+  if (!(angleValue != NaN && angleValue <= 90 && angleValue >= 0)) {
+    alert(`Zadejte prosim cislo od 0 do 90, Vaše hodnota je ${angleValue}`);
+  } else {
+    angleTextInput.value = "";
+    draw((angleValue * (canvas.width / 2)) / 90 + canvas.width / 2);
+    return;
   }
 }
 
-angleTextInput.addEventListener('click', () => {
+angleSendButton.addEventListener("click", () => {
   readingAngleValue(angleTextInput);
 });
 
-angleSendButton.addEventListener('enter', () => {
-  readingAngleValue(angleTextInput);
+angleTextInput.addEventListener("keydown", (event) => {
+  if (event.key == "Enter") {
+    readingAngleValue(angleTextInput);
+  }
 });
 canvas.width = width;
 canvas.height = height;
 
-draw();
+draw(900);
 
-console.log(canvas.getContext('2d'));
+console.log(canvas.getContext("2d"));
 
-function draw() {
-  ctx.fillStyle = 'lightblue';
+function draw(xAxis: number) {
+  ctx.fillStyle = "lightblue";
   ctx.fillRect(0, 0, width, height / 2);
-  ctx.fillStyle = 'lightgreen';
+  ctx.fillStyle = "lightgreen";
   ctx.fillRect(0, height / 2, width, height / 2);
-  ctx.strokeStyle = 'red';
+  ctx.strokeStyle = "red";
   ctx.beginPath();
   ctx.moveTo(300, 0);
   ctx.lineTo(500, height / 2);
   ctx.stroke();
-  ctx.strokeStyle = 'blue';
+  ctx.strokeStyle = "blue";
   ctx.beginPath();
   ctx.moveTo(500, height / 2);
-  ctx.lineTo(900, height);
+  ctx.lineTo(xAxis, height);
   ctx.stroke();
-
-};
+}
