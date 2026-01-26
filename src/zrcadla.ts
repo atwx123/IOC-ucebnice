8cc9a067f2e31e2a23236c86a07f66a94521347c
atwx123 <atex.123@seznam.cz>
2026-01-26 19:46:28 +0100

    .

--- src/zrcadla.ts
+++ src/zrcadla.ts
@@ -1,12 +1,16 @@
 const canvas: HTMLCanvasElement = document.getElementById(
   "zrcadlaInt",
 ) as HTMLCanvasElement;
+const ctx: CanvasRenderingContext2D = canvas.getContext(
+  "2d",
+) as CanvasRenderingContext2D;
 const width: number = 1000;
 const height: number = 600;
 const center = width / 2;
 const rz: number = 400;
-const ohnisko = width / 2 + rz / 2;
-const pOhnisko = rz / 2;
+const ohnisko: number = width / 2 + rz / 2;
+const pOhnisko: number = rz / 2;
+const heightIcon: number = 200;
 
 function verBeam(y: number, xk?: number, color?: string) {
   ctx.save();
@@ -26,12 +30,28 @@ function verBeam(y: number, xk?: number, color?: string) {
   ctx.stroke();
   ctx.restore();
 }
+
+function arcInterVert(): number {
+  return Math.sqrt(Math.pow(rz, 2) - Math.pow(heightIcon, 2));
+}
+
+function arcInterCenter(): [number, number] {
+  let alpha: number = Math.asin(heightIcon / Math.abs(center));
+  return [rz * Math.cos(alpha), rz * Math.sin(alpha)];
+}
+
+function linesInter(): number {
+  let y: number = arcInterCenter()[1];
+  return y / Math.tan(Math.asin(heightIcon / rz));
+}
+
+function interEdge(): [number, number] {
+  let y: number = Math.tan(Math.asin(heightIcon / rz) * -center);
+  return [-center, y];
+}
+
 canvas.width = width;
 canvas.height = height;
-
-let ctx: CanvasRenderingContext2D = canvas.getContext(
-  "2d",
-) as CanvasRenderingContext2D;
 
 ctx.setLineDash([5, 10, 10, 15]);
 ctx.beginPath();
@@ -46,6 +66,7 @@ ctx.beginPath();
 ctx.arc(0, height / 2, rz, Math.PI / 2, (Math.PI * 2) / 1.5, true);
 ctx.stroke();
 
+ctx.save();
 ctx.beginPath();
 ctx.moveTo(0, height / 2 - 5);
 ctx.lineTo(0, height / 2 + 5);
@@ -61,3 +82,6 @@ ctx.lineTo(pOhnisko, height / 2 + 5);
 ctx.stroke();
 
 ctx.fillText("F", pOhnisko, height / 2 + 10);
+ctx.restore();
+
+verBeam(height / 2 - heightIcon, arcInterVert(), "red");
