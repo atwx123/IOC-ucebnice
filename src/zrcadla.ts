bf31a6e39090eb2560034a713b9584493f20969c
Alice Dědicová <atex.123@seznam.cz>
2026-02-20 10:37:01 +0100

    added types

--- src/zrcadla.ts
+++ src/zrcadla.ts
@@ -19,7 +19,7 @@ const pOhnisko: number = rz / 2;
 const iconHeight: number = 50;
 const drawIconHeight = height / 2 - iconHeight;
 let iconX: number = -200;
-let obraz = new Image();
+let obraz: HTMLImageElement = new Image();
 
 function horBeam(y: number, xk?: number, color?: string) {
   ctx.save();
@@ -43,38 +43,38 @@ function horBeam(y: number, xk?: number, color?: string) {
 }
 
 function getRedHit(): [number, number] {
-  const y = drawIconHeight;
-  const dy = y - height / 2;
-  const x = Math.sqrt(Math.pow(rz, 2) - Math.pow(dy, 2));
+  const y: number = drawIconHeight;
+  const dy: number = y - height / 2;
+  const x: number = Math.sqrt(Math.pow(rz, 2) - Math.pow(dy, 2));
   return [x, y];
 }
 
 function getBlueStart(): [number, number] {
-  const objX = iconX;
-  const objY = drawIconHeight;
-  const focusX = pOhnisko;
-  const focusY = height / 2;
+  const objX: number = iconX;
+  const objY: number = drawIconHeight;
+  const focusX: number = pOhnisko;
+  const focusY: number = height / 2;
 
-  const relObjY = objY - height / 2;
-  const relFocusY = 0; // Focus is on the axis
+  const relObjY: number = objY - height / 2;
+  const relFocusY: number = 0; // Focus is on the axis
 
   if (Math.abs(objX - focusX) < 0.01) {
-    const x = focusX;
-    const val = Math.sqrt(rz * rz - x * x);
-    const relHitY = relObjY < 0 ? val : -val;
+    const x: number = focusX;
+    const val: number = Math.sqrt(rz * rz - x * x);
+    const relHitY: number = relObjY < 0 ? val : -val;
     return [x, relHitY + height / 2];
   }
 
-  const m = (relFocusY - relObjY) / (focusX - objX);
+  const m: number = (relFocusY - relObjY) / (focusX - objX);
 
-  const A = 1 + m * m;
-  const B = -2 * focusX * m * m;
-  const C = m * m * focusX * focusX - rz * rz;
+  const A: number = 1 + m * m;
+  const B: number = -2 * focusX * m * m;
+  const C: number = m * m * focusX * focusX - rz * rz;
 
-  const delta = Math.sqrt(B * B - 4 * A * C);
-  const x = (-B + delta) / (2 * A);
+  const delta: number = Math.sqrt(B * B - 4 * A * C);
+  const x: number = (-B + delta) / (2 * A);
 
-  const relHitY = m * (x - focusX);
+  const relHitY: number = m * (x - focusX);
 
   return [x, relHitY + height / 2];
 }
@@ -82,34 +82,34 @@ function getBlueStart(): [number, number] {
 function linesInter(): [number, number] {
   const [redHitX, redHitY] = getRedHit();
   const [blueHitX, blueHitY] = getBlueStart();
-  const focusX = pOhnisko;
-  const focusY = height / 2;
+  const focusX: number = pOhnisko;
+  const focusY: number = height / 2;
 
-  const mRed = (focusY - redHitY) / (focusX - redHitX);
-  const targetY = blueHitY;
-  const targetX = focusX + (targetY - focusY) / mRed;
+  const mRed: number = (focusY - redHitY) / (focusX - redHitX);
+  const targetY: number = blueHitY;
+  const targetX: number = focusX + (targetY - focusY) / mRed;
 
   return [targetX, targetY];
 }
 
 function interEdgeBot(): [number, number] {
   const [hitX, hitY] = getRedHit();
-  const focusX = pOhnisko;
-  const focusY = height / 2;
-  const slope = (focusY - hitY) / (focusX - hitX);
-  const xTarget = -center;
-  const yTarget = focusY + slope * (xTarget - focusX);
+  const focusX: number = pOhnisko;
+  const focusY: number = height / 2;
+  const slope: number = (focusY - hitY) / (focusX - hitX);
+  const xTarget: number = -center;
+  const yTarget: number = focusY + slope * (xTarget - focusX);
   return [xTarget, yTarget];
 }
 
 function getBlueHit(): [number, number] {
-  const objX = iconX;
-  const objY = drawIconHeight;
-  const focusX = pOhnisko;
-  const focusY = height / 2;
-  const slope = (focusY - objY) / (focusX - objX);
-  const xTarget = -center;
-  const yTarget = focusY + slope * (xTarget - focusX);
+  const objX: number = iconX;
+  const objY: number = drawIconHeight;
+  const focusX: number = pOhnisko;
+  const focusY: number = height / 2;
+  const slope: number = (focusY - objY) / (focusX - objX);
+  const xTarget: number = -center;
+  const yTarget: number = focusY + slope * (xTarget - focusX);
   return [xTarget, yTarget];
 }
 
@@ -159,8 +159,8 @@ function drawToF() {
   drawBackground();
   ctx.translate(center, 0);
 
-  const w = obraz.width;
-  const newWidth = w / (obraz.height / iconHeight);
+  const w: number = obraz.width;
+  const newWidth: number = w / (obraz.height / iconHeight);
   if (obraz.complete) {
     let leftSide = iconX - newWidth / 2;
     ctx.drawImage(
@@ -172,8 +172,8 @@ function drawToF() {
     );
   }
 
-  const rightEdge = 1000;
-  const leftEdge = -center;
+  const rightEdge: number = 1000;
+  const leftEdge: number = -center;
 
   // Red ray
   ctx.strokeStyle = "red";
@@ -181,12 +181,12 @@ function drawToF() {
 
   horBeam(redHit[1], redHit[0], "red");
 
-  const focusX = pOhnisko;
-  const focusY = height / 2;
-  const slopeRed = (focusY - redHit[1]) / (focusX - redHit[0]);
+  const focusX: number = pOhnisko;
+  const focusY: number = height / 2;
+  const slopeRed: number = (focusY - redHit[1]) / (focusX - redHit[0]);
 
-  const yAtLeft = redHit[1] + slopeRed * (leftEdge - redHit[0]);
-  const yAtRight = redHit[1] + slopeRed * (rightEdge - redHit[0]);
+  const yAtLeft: number = redHit[1] + slopeRed * (leftEdge - redHit[0]);
+  const yAtRight: number = redHit[1] + slopeRed * (rightEdge - redHit[0]);
 
   ctx.beginPath();
   ctx.moveTo(redHit[0], redHit[1]);
@@ -205,14 +205,14 @@ function drawToF() {
   // Blue Ray
   ctx.strokeStyle = "blue";
 
-  const blueMirrorHit = getBlueStart();
+  const blueMirrorHit: [number, number] = getBlueStart();
 
-  const objX = iconX;
-  const objY = drawIconHeight;
-  const slopeBlue = (focusY - objY) / (focusX - objX);
+  const objX: number = iconX;
+  const objY: number = drawIconHeight;
+  const slopeBlue: number = (focusY - objY) / (focusX - objX);
 
-  const blueYAtLeft = focusY + slopeBlue * (leftEdge - focusX);
-  const blueYAtRight = focusY + slopeBlue * (rightEdge - focusX);
+  const blueYAtLeft: number = focusY + slopeBlue * (leftEdge - focusX);
+  const blueYAtRight: number = focusY + slopeBlue * (rightEdge - focusX);
 
   ctx.setLineDash([]);
   ctx.beginPath();
@@ -250,12 +250,12 @@ function drawToF() {
     ctx.drawImage(img, -width / 2, -height / 2, width, height);
     ctx.restore();
   };
-  const intersection = linesInter();
-  const linesInterX = intersection[0];
-  const linesInterY = intersection[1];
+  const intersection: [number, number] = linesInter();
+  const linesInterX: number = intersection[0];
+  const linesInterY: number = intersection[1];
 
-  const reflectIconHeight = linesInterY - height / 2;
-  const reflectNewWidth =
+  const reflectIconHeight: number = linesInterY - height / 2;
+  const reflectNewWidth: number =
     (obraz.width / obraz.height) * Math.abs(reflectIconHeight);
   drawRotatedImage(
     ctx,
@@ -279,8 +279,8 @@ function drawBehindF() {
   drawBackground();
   ctx.translate(center, 0);
 
-  const w = obraz.width;
-  const newWidth = w / (obraz.height / iconHeight);
+  const w: number = obraz.width;
+  const newWidth: number = w / (obraz.height / iconHeight);
   if (obraz.complete) {
     let leftSide = iconX - newWidth / 2;
     ctx.drawImage(
@@ -291,102 +291,12 @@ function drawBehindF() {
       iconHeight,
     );
   }
-
-  const rightEdge = 1000;
-  const leftEdge = -center;
-
-  // Red ray
-  ctx.strokeStyle = "red";
-  const redHit = getRedHit();
-
-  horBeam(redHit[1], redHit[0], "red");
-
-  const focusX = pOhnisko;
-  const focusY = height / 2;
-  const slopeRed = (focusY - redHit[1]) / (focusX - redHit[0]);
-
-  const yAtLeft = redHit[1] + slopeRed * (leftEdge - redHit[0]);
-  const yAtRight = redHit[1] + slopeRed * (rightEdge - redHit[0]);
-
-  ctx.beginPath();
-  ctx.moveTo(redHit[0], redHit[1]);
-  ctx.lineTo(leftEdge, yAtLeft);
-  ctx.stroke();
-
-  ctx.save();
-  ctx.beginPath();
-  ctx.setLineDash([5, 5]);
-  ctx.moveTo(redHit[0], redHit[1]);
-  ctx.lineTo(rightEdge, yAtRight);
-  ctx.stroke();
-  ctx.setLineDash([]);
-  ctx.restore();
-
-  // Blue Ray
-  ctx.strokeStyle = "blue";
-
-  const blueMirrorHit = getBlueStart();
-
-  const objX = iconX;
-  const objY = drawIconHeight;
-  const slopeBlue = (focusY - objY) / (focusX - objX);
-
-  const blueYAtLeft = focusY + slopeBlue * (leftEdge - focusX);
-  const blueYAtRight = focusY + slopeBlue * (rightEdge - focusX);
-
-  ctx.setLineDash([]);
-  ctx.beginPath();
-  ctx.moveTo(leftEdge, blueYAtLeft);
-  ctx.lineTo(blueMirrorHit[0], blueMirrorHit[1]);
-  ctx.stroke();
-
-  ctx.save();
-  ctx.beginPath();
-  ctx.setLineDash([5, 5]);
-  ctx.moveTo(blueMirrorHit[0], blueMirrorHit[1]);
-  ctx.lineTo(rightEdge, blueYAtRight);
-  ctx.stroke();
-  ctx.setLineDash([]);
-  ctx.restore();
-
-  horBeam(blueMirrorHit[1], blueMirrorHit[0], "blue");
-
-  // Obraz
-  const reflection: HTMLImageElement = obraz;
-  reflection.src = obraz.src;
-  const drawRotatedImage = (
-    ctx: CanvasRenderingContext2D,
-    img: HTMLImageElement,
-    x: number,
-    y: number,
-    width: number,
-    height: number,
-    degrees: number,
-  ): void => {
-    ctx.save();
-    ctx.translate(x + width / 2, y + height / 2);
-    const radians = (degrees * Math.PI) / 180;
-    ctx.rotate(radians);
-    ctx.drawImage(img, -width / 2, -height / 2, width, height);
-    ctx.restore();
-  };
-  const intersection = linesInter();
-  const linesInterX = intersection[0];
-  const linesInterY = intersection[1];
-
-  const reflectIconHeight = linesInterY - height / 2;
-  const reflectNewWidth =
-    (obraz.width / obraz.height) * Math.abs(reflectIconHeight);
-  drawRotatedImage(
-    ctx,
-    reflection,
-    linesInterX - reflectNewWidth / 2,
-    height / 2,
-    reflectNewWidth,
-    reflectIconHeight,
-    180,
-  );
 }
+canvas.width = width;
+canvas.height = height;
+
+iconInput.max = (center - 10).toString();
+
 iconInput.addEventListener("input", () => {
   iconX = -iconInput.valueAsNumber;
   iconOutput.value = iconInput.value;
