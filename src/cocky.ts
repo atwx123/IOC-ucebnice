1fba0ff1e121cdd2169455a95b19097efb65ecc8
atwx123 <atex.123@seznam.cz>
2026-03-21 09:23:08 +0100

    rozptylka

--- src/cocky.ts
+++ src/cocky.ts
@@ -7,25 +7,49 @@ const ctx: CanvasRenderingContext2D = canvas.getContext(
 const iconXInput: HTMLInputElement = document.getElementById(
   "objectXInput",
 ) as HTMLInputElement;
+const buttonConc: HTMLButtonElement = document.getElementById(
+  "buttonConc",
+) as HTMLButtonElement;
+const buttonConv: HTMLButtonElement = document.getElementById(
+  "buttonConv",
+) as HTMLButtonElement;
 const width: number = 1000;
 const height: number = 600;
 const center: number = width / 2;
 const rConv: number = 200;
 const n: number = 1.7;
 const f: number = 1 / (((n - 1) * 2) / rConv);
-const v: number = 150;
+const vConv: number = 150;
+const lensWidthConv: number = 40;
+const lensHeightConv: number = 260;
+const pinchConv: number = 15;
 let iconX: number = 200;
 const obraz: HTMLImageElement = new Image();
-const iconHeightConv: number = 100;
+const iconHeight: number = 100;
+const drawIconHeight: number = height / 2 - iconHeight;
+let lens: boolean = true;
+
+function drawMarker(text: string, x: number) {
+  ctx.save();
+  ctx.beginPath();
+  ctx.moveTo(x, height / 2 - 5);
+  ctx.lineTo(x, height / 2 + 5);
+  ctx.stroke();
+  ctx.textAlign = "center";
+  ctx.textBaseline = "hanging";
+  ctx.font = "20px sans-serif";
+  ctx.fillText(text, x, height / 2 + 10);
+  ctx.restore();
+}
 
 function drawBackgroundConv() {
-  const angle = Math.acos(v / rConv);
+  const angle = Math.acos(vConv / rConv);
   ctx.save();
   ctx.fillStyle = "#7FE1EE";
   ctx.beginPath();
-  ctx.arc(center - v, height / 2, rConv, -angle, angle, false);
+  ctx.arc(center - vConv, height / 2, rConv, -angle, angle, false);
   ctx.arc(
-    center + v,
+    center + vConv,
     height / 2,
     rConv,
     Math.PI - angle,
@@ -44,38 +68,11 @@ function drawBackgroundConv() {
   ctx.stroke();
   ctx.setLineDash([]);
 
-  ctx.save();
-  ctx.beginPath();
-  ctx.moveTo(center - f, height / 2 - 5);
-  ctx.lineTo(center - f, height / 2 + 5);
-  ctx.stroke();
-  ctx.textAlign = "center";
-  ctx.textBaseline = "hanging";
-  ctx.font = "20px sans-serif";
-  ctx.fillText("F", center - f, height / 2 + 10);
-  ctx.restore();
+  drawMarker("F", center - f);
 
-  ctx.save();
-  ctx.beginPath();
-  ctx.moveTo(center + f, height / 2 - 5);
-  ctx.lineTo(center + f, height / 2 + 5);
-  ctx.stroke();
-  ctx.textAlign = "center";
-  ctx.textBaseline = "hanging";
-  ctx.font = "20px sans-serif";
-  ctx.fillText("F'", center + f, height / 2 + 10);
-  ctx.restore();
+  drawMarker("F'", center + f);
 
-  ctx.save();
-  ctx.beginPath();
-  ctx.moveTo(center, height / 2 - 5);
-  ctx.lineTo(center, height / 2 + 5);
-  ctx.stroke();
-  ctx.textAlign = "center";
-  ctx.textBaseline = "hanging";
-  ctx.font = "20px sans-serif";
-  ctx.fillText("O", center, height / 2 + 10);
-  ctx.restore();
+  drawMarker("O", center);
 }
 
 function drawXgt2f() {
@@ -85,36 +82,34 @@ function drawXgt2f() {
   ctx.restore();
 
   ctx.save();
-  const newWidth = (iconHeightConv * obraz.width) / obraz.height;
+  const newWidth = (iconHeight * obraz.width) / obraz.height;
   drawBackgroundConv();
   if (obraz.complete && obraz.height > 0) {
     ctx.drawImage(
       obraz,
       iconX - newWidth / 2,
-      height / 2 - iconHeightConv,
+      drawIconHeight,
       newWidth,
-      iconHeightConv,
+      iconHeight,
     );
   }
 
-  const blueSlope = iconHeightConv / (center - iconX - f);
+  const blueSlope = iconHeight / (center - iconX - f);
   const yLens = height / 2 + blueSlope * f;
 
-  const redSlope = iconHeightConv / f;
+  const redSlope = iconHeight / f;
 
-  const intersectX =
-    center + (yLens - (height / 2 - iconHeightConv)) / redSlope;
+  const intersectX = center + (yLens - drawIconHeight) / redSlope;
 
   const drawLimitX = Math.max(width, intersectX);
-  const redEndY =
-    height / 2 - iconHeightConv + redSlope * (drawLimitX - center);
+  const redEndY = drawIconHeight + redSlope * (drawLimitX - center);
 
   // --- Draw Red Ray ---
   ctx.save();
   ctx.strokeStyle = "red";
   ctx.beginPath();
-  ctx.moveTo(0, height / 2 - iconHeightConv);
-  ctx.lineTo(center, height / 2 - iconHeightConv);
+  ctx.moveTo(0, drawIconHeight);
+  ctx.lineTo(center, drawIconHeight);
   ctx.lineTo(drawLimitX, redEndY);
   ctx.stroke();
   ctx.restore();
@@ -123,7 +118,7 @@ function drawXgt2f() {
   ctx.save();
   ctx.strokeStyle = "blue";
   ctx.beginPath();
-  ctx.moveTo(iconX, height / 2 - iconHeightConv);
+  ctx.moveTo(iconX, drawIconHeight);
   ctx.lineTo(center, yLens);
   ctx.lineTo(drawLimitX, yLens); // Extends all the way to the limit
   ctx.stroke();
@@ -168,21 +163,21 @@ function drawXeqf() {
   ctx.restore();
 
   ctx.save();
-  const newWidth = (iconHeightConv * obraz.width) / obraz.height;
+  const newWidth = (iconHeight * obraz.width) / obraz.height;
   drawBackgroundConv();
   if (obraz.complete && obraz.height > 0) {
     ctx.drawImage(
       obraz,
       iconX - newWidth / 2,
-      height / 2 - iconHeightConv,
+      drawIconHeight,
       newWidth,
-      iconHeightConv,
+      iconHeight,
     );
   }
-  const topY = height / 2 - iconHeightConv;
+  const topY = drawIconHeight;
 
   // Red ray
-  const redSlope = iconHeightConv / f;
+  const redSlope = iconHeight / f;
   const redEndY = topY + redSlope * (width - center);
 
   ctx.save();
@@ -195,7 +190,7 @@ function drawXeqf() {
   ctx.restore();
 
   // Blue ray
-  const blueSlope = iconHeightConv / f;
+  const blueSlope = iconHeight / f;
   const bCenter = height / 2 - blueSlope * center;
 
   const ybl = bCenter;
@@ -217,21 +212,21 @@ function drawXltf() {
   ctx.restore();
 
   drawBackgroundConv();
-  const newWidth = (iconHeightConv * obraz.width) / obraz.height;
+  const newWidth = (iconHeight * obraz.width) / obraz.height;
   if (obraz.complete && obraz.height > 0) {
     ctx.drawImage(
       obraz,
       iconX - newWidth / 2,
-      height / 2 - iconHeightConv,
+      drawIconHeight,
       newWidth,
-      iconHeightConv,
+      iconHeight,
     );
   }
 
-  const topY = height / 2 - iconHeightConv;
+  const topY = drawIconHeight;
 
-  const redSlope = iconHeightConv / f;
-  const blueSlope = iconHeightConv / (center - iconX);
+  const redSlope = iconHeight / f;
+  const blueSlope = iconHeight / (center - iconX);
 
   const intersectX = center + (topY - height / 2) / (blueSlope - redSlope);
   const intersectY = height / 2 + blueSlope * (intersectX - center);
@@ -288,21 +283,153 @@ function drawXltf() {
   }
 }
 
+function drawBackgroundConc() {
+  ctx.save();
+  ctx.fillStyle = "#7FE1EE";
+  ctx.beginPath();
+
+  ctx.moveTo(center - lensWidthConv / 2, height / 2 - lensHeightConv / 2);
+
+  ctx.lineTo(center + lensWidthConv / 2, height / 2 - lensHeightConv / 2);
+
+  ctx.quadraticCurveTo(
+    center + lensWidthConv / 2 - pinchConv,
+    height / 2,
+    center + lensWidthConv / 2,
+    height / 2 + lensHeightConv / 2,
+  );
+
+  ctx.lineTo(center - lensWidthConv / 2, height / 2 + lensHeightConv / 2);
+
+  ctx.quadraticCurveTo(
+    center - lensWidthConv / 2 + pinchConv,
+    height / 2,
+    center - lensWidthConv / 2,
+    height / 2 - lensHeightConv / 2,
+  );
+
+  ctx.closePath();
+  ctx.fill();
+  ctx.stroke();
+  ctx.restore();
+
+  ctx.beginPath();
+  ctx.setLineDash([5, 5]);
+  ctx.moveTo(0, height / 2);
+  ctx.lineTo(width, height / 2);
+  ctx.stroke();
+  ctx.setLineDash([]);
+
+  drawMarker("F", center - f);
+  drawMarker("F'", center + f);
+  drawMarker("O", center);
+}
+function drawConc() {
+  ctx.save();
+  ctx.setTransform(1, 0, 0, 1, 0, 0);
+  ctx.clearRect(0, 0, width, height);
+  ctx.restore();
+  drawBackgroundConc();
+
+  const newWidth = (iconHeight * obraz.width) / obraz.height;
+  if (obraz.complete && obraz.height > 0) {
+    ctx.drawImage(
+      obraz,
+      iconX - newWidth / 2,
+      drawIconHeight,
+      newWidth,
+      iconHeight,
+    );
+  }
+
+  // Red ray
+  ctx.save();
+  ctx.strokeStyle = "red";
+  ctx.beginPath();
+  ctx.moveTo(iconX, drawIconHeight);
+  ctx.lineTo(center, drawIconHeight);
+  ctx.stroke();
+
+  const redSlope = -iconHeight / f;
+  const redB = drawIconHeight - redSlope * center;
+
+  ctx.beginPath();
+  ctx.setLineDash([5, 5]);
+  ctx.moveTo(center - f, height / 2);
+  ctx.lineTo(center, drawIconHeight);
+  ctx.stroke();
+
+  ctx.beginPath();
+  ctx.setLineDash([]);
+  ctx.moveTo(center, drawIconHeight);
+  const redEndY = redSlope * width + redB;
+  ctx.lineTo(width, redEndY);
+  ctx.stroke();
+  ctx.restore();
+
+  // 2. BLUE RAY
+
+  const blueSlope = iconHeight / (center - iconX);
+  const blueB = height / 2 - blueSlope * center;
+
+  ctx.save();
+  ctx.strokeStyle = "blue";
+  ctx.beginPath();
+  ctx.moveTo(iconX, drawIconHeight);
+  const blueEndY = blueSlope * width + blueB;
+  ctx.lineTo(width, blueEndY);
+  ctx.stroke();
+  ctx.restore();
+
+  const intersectX =
+    center + (drawIconHeight - height / 2) / (blueSlope - redSlope);
+  const intersectY = height / 2 + blueSlope * (intersectX - center);
+
+  // obraz
+  if (obraz.complete && obraz.height > 0) {
+    const virtualHeight = height / 2 - intersectY;
+    const aspect = obraz.width / obraz.height;
+    const virtualWidth = virtualHeight * aspect;
+
+    ctx.save();
+    ctx.globalAlpha = 0.4;
+    ctx.drawImage(
+      obraz,
+      intersectX - virtualWidth / 2,
+      intersectY,
+      virtualWidth,
+      virtualHeight,
+    );
+    ctx.restore();
+  }
+}
 canvas.width = width;
 canvas.height = height;
 drawBackgroundConv();
 
 iconXInput.addEventListener("input", () => {
   iconX = iconXInput.valueAsNumber;
-  const distance = center - iconX;
-  if (Math.abs(distance - f) < 1.5) {
-    drawXeqf();
-  } else if (distance > f) {
-    drawXgt2f();
+  if (lens) {
+    const distance = center - iconX;
+    if (Math.abs(distance - f) < 1.5) {
+      drawXeqf();
+    } else if (distance > f) {
+      drawXgt2f();
+    } else {
+      drawXltf();
+    }
   } else {
-    drawXltf();
+    drawConc();
   }
 });
+
+buttonConc.addEventListener("click", () => {
+  lens = false;
+});
+buttonConv.addEventListener("click", () => {
+  lens = true;
+});
+
 obraz.onload = () => {
   drawXgt2f();
 };
